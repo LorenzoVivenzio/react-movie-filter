2aba5045fb6b22497c9ac94ae87750617103c41b
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2025-12-02 16:45:38 +0100

    add lista film

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,36 @@ function App() {
 
   return (
     <>
-    <header>
-      <h1 className='text-center'>Film</h1>
-    </header>
+      <div className="container">
+        <header className='d-flex justify-content-between mt-3 mb-3'>
+          <h1>Film</h1>
+          <nav class="navbar ">
+            <div class="container-fluid">
+              <form 
+              class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button 
+                class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+        </header>
 
-  
+
+
+        <div className="row">
+          {film.map((film, index) => {
+            return (
+              <div className="col-3 border mt-3 me-1">
+                <h2>{film.title}</h2>
+                <p>{film.genre}</p>
+              </div>
+            )
+          }
+          )}
+        </div>
+      </div>
+
     </>
   )
 }
